dda7504886aa8875804a27ad2ce966c1c063c648
diaskrv <103264544+diaskrv@users.noreply.github.com>
2024-05-10 15:17:30 +0500

    nothing

--- windT/views.py
+++ windT/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+import tensorflow as tf
+import pandas as pd
+from keras.models import load_model
 
 def index(request):
     return render(request, 'turbo/index.html')
